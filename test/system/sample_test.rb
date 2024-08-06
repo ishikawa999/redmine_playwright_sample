92b25af4c56ce58cc23b7e2e37e4ad73912e90af
ishikawa <ishikawa@farend.jp>
2024-08-06 07:26:48 +0000

    Add fail test

--- test/system/sample_test.rb
+++ test/system/sample_test.rb
@@ -12,10 +12,19 @@ class SampleTest < PlaywrightSystemTestCase
     log_user('admin', 'admin')
     visit '/issues/new'
 
+    assert_text 'New issue'
     fill_in 'Subject', with: 'issue subject'
     click_on 'Create'
 
     assert_text 'issue subject'
     assert_text "Issue ##{Issue.last.id} created."
+  end
+
+  test 'should fail' do
+    log_user('admin', 'admin')
+    visit '/issues/new'
+
+    fill_in 'Subject', with: 'issue subject'
+    assert false # 必ず失敗する
   end
 end
