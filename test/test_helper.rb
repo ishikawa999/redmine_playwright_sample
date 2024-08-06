70f7a5b228a5cf21f17fffde596f4f3cde7a4e39
ishikawa <ishikawa@farend.jp>
2024-08-06 07:09:53 +0000

    Add system test options

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,10 @@ require_relative '../../../test/test_helper'
 class PlaywrightSystemTestCase < ActionDispatch::SystemTestCase
   driven_by(:playwright, options: { headless: ENV['TEST_HEADLESS'] == 'false' ? false : true, browser_type: :chromium })
 
+  Capybara.default_max_wait_time = 10
+  Capybara.default_driver = :playwright
+  Capybara.javascript_driver = :playwright
+
   def log_user(login, password)
     visit '/my/page'
     assert_equal '/login', current_path
